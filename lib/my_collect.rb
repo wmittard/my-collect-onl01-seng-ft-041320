a00def529755e71c52ecf6b71c6351bd8364d283
William Mittard <billymittard@ymail.com>
2020-04-15 21:15:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collection(array)
   i = 0 
  new_collection = []
  while i < collection.length
-  new_collection << (array[i])
+  new_collection << yield (collection[i])
   i += 1 
 end
 collection 
